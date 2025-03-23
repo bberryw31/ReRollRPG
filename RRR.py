cb7210f67466762a38cf1f44b2b484190c050186
JIMMY CHO <bberryw31@gmail.com>
2025-03-23 14:50:41 -0700

    Update game_intro function

--- RRR.py
+++ RRR.py
@@ -1,9 +1,16 @@
 import time
 
+import random
+
+
+def clear_screen(stage):
+    pass
+
 
 def game_intro():
     print("\n\033[92m                            🎲 Welcome to ReRollRPG 🎲\033[0m")
     print("\n\033[2m   Type 'start' to begin your adventure.\033[0m")
+    error_counter = 0
     while True:
         user_input = input("\n> ").strip().lower()
         if user_input == "start" or user_input == "s":
@@ -14,7 +21,21 @@ def game_intro():
                 time.sleep(0.5)
             return True
         else:
-            print("Invalid input. Type 'start' to begin.")
+            if error_counter < 3:
+                response = [
+                    "Invalid input. Type 'start' to begin.",
+                    "Unknown command. Type 'start' to begin."
+                ]
+                error_counter += 1
+            else:
+                response = [
+                    "Excuse me? Type 'start' to begin.",
+                    "Stop playing with me. Type 'start' to begin.",
+                    "Lord have mercy.. Type 'start' to begin.",
+                    "Are you kidding me? Type 'start' to begin.",
+                    "I'm about to lose it. Type 'start' to begin.",
+                ]
+            print(random.choice(response))
 
 def game():
     """
