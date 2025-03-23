d9855f865c825ed3a7151c253cd1a2572aa64680
JIMMY CHO <bberryw31@gmail.com>
2025-03-23 14:43:28 -0700

    Add game_intro function

---
+++ RRR.py
@@ -0,0 +1,28 @@
+import time
+
+
+def game_intro():
+    print("\n\033[92m                            🎲 Welcome to ReRollRPG 🎲\033[0m")
+    print("\n\033[2m   Type 'start' to begin your adventure.\033[0m")
+    while True:
+        user_input = input("\n> ").strip().lower()
+        if user_input == "start" or user_input == "s":
+            start_counter = 1
+            while start_counter < 6:
+                print("\n\033[0mLoading game" + ("." * start_counter))
+                start_counter += 1
+                time.sleep(0.5)
+            return True
+        else:
+            print("Invalid input. Type 'start' to begin.")
+
+def game():
+    """
+    Drive the game.
+    """
+    game_intro()
+
+
+if __name__ == "__main__":
+    game()
+
