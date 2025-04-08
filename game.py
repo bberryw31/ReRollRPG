b907f1ed0994a5b276ae3275b162da2b1255de04
JIMMY CHO <bberryw31@gmail.com>
2025-04-07 23:03:48 -0700

    Increase chances of getting poisoned in drink_water function

--- game.py
+++ game.py
@@ -521,15 +521,15 @@ def drink_water(character: dict):
                           "\033[0m > ").strip().lower()
     if user_decision == "y":
         odds = random.randint(1, 100)
-        if odds < 25 + character["stats"]["int"] * 0.5:
+        if odds < 20 + character["stats"]["int"] * 0.5:
             random_stat = random.choice(["str", "dex", "int", "luc"])
             random_change = random.randint(1, 2)
             character["stats"][random_stat] += random_change
             print(f"\033[94mYou feel stronger. \033[0m\n"
                   f"\t\033[1mGain \033[96m+{random_change}\033[0m \033[1m{random_stat.upper()}\033[0m!")
-        elif odds < 50 + character["stats"]["int"]:
+        elif odds < 40 + character["stats"]["int"]:
             random_increase = random.randint(1, 2)
-            character["HP"] += random_increase
+            character["HP"] = min(character["max_HP"], character["HP"] + random_increase)
             print(f"\033[93mYou feel refreshed. \033[0m\n"
                   f"\t\033[1mHeal \033[96m{random_increase}\033[0m \033[1mHP\033[0m!")
         else:
@@ -537,6 +537,7 @@ def drink_water(character: dict):
             print(f"\033[91mYou have been poisoned! \033[0m\n"
                   f"\t\033[1mLose \033[96m1\033[0m \033[1mHP\033[0m")
         time.sleep(2)
+
 
 
 def open_reward(character: dict) -> bool:
